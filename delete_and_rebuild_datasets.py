fba6004634f3bea367ffedb90898eeadec8ca066
Yngve Høiseth <yhoiseth@gmail.com>
2017-04-01 19:42:49 +0200

    Populate output table

--- delete_and_rebuild_datasets.py
+++ delete_and_rebuild_datasets.py
@@ -2,6 +2,7 @@ from google.cloud import bigquery
 from pprint import pprint
 import sys
 
+
 project_name = sys.argv[1]
 client = bigquery.Client(project_name)
 
@@ -50,15 +51,21 @@ def create_and_populate_input_tables():
                 '.' + \
                 table_name
 
-            values = '("A value"), ("A second value"), ("A third value")'
+            values = [
+                '("A value from %s.%s")' % (dataset.name, table_name),
+                '("A second value from %s.%s")' % (dataset.name, table_name),
+                '("A third value from %s.%s")' % (dataset.name, table_name),
+            ]
 
-            query = client.run_sync_query(
-                'INSERT INTO `%s` (`column_1`) VALUES %s' % (query_table_name, values)
-            )
+            query = 'INSERT INTO `%s` (`column_1`) VALUES %s' % (query_table_name, ', '.join(values))
 
-            query.use_legacy_sql = False
+            run_query(query)
 
-            query.run()
+
+def run_query(query):
+    query_results = client.run_sync_query(query)
+    query_results.use_legacy_sql = False
+    query_results.run()
 
 
 def create_table(dataset, name, schema_fields):
@@ -75,12 +82,52 @@ def create_output_table():
 
     schema_fields = [
         bigquery.SchemaField('column_1', 'STRING'),
-        bigquery.SchemaField('country_code', 'STRING'),
         bigquery.SchemaField('property_id', 'STRING'),
         bigquery.SchemaField('property_name', 'STRING'),
+        bigquery.SchemaField('country_code', 'STRING'),
     ]
 
     create_table(dataset, 'output_table', schema_fields)
+
+
+def populate_output_table():
+    properties = get_properties()
+
+    for property in properties:
+        dataset = client.dataset(property['name'])
+
+        tables = dataset.list_tables()
+
+        for table in tables:
+            query_parts = [
+                'INSERT INTO `%s.output.output_table`' % (project_name),
+                '(column_1, property_id, property_name, country_code)',
+                'SELECT column_1, "%s", "%s", "%s"' % (
+                    property['id'],
+                    property['name'],
+                    property['country_code']
+                ),
+                'FROM `%s.%s.%s`' % (project_name, dataset.name, table.name),
+            ]
+
+            query = ' '.join(query_parts)
+
+            run_query(query)
+
+
+def get_properties():
+    return [
+        {
+            'id': '1',
+            'name': 'property_1',
+            'country_code': 'uk',
+        },
+        {
+            'id': '2',
+            'name': 'property_2',
+            'country_code': 'nz'
+        },
+    ]
 
 
 def main():
@@ -89,5 +136,6 @@ def main():
     create_and_populate_input_tables()
     create_output_dataset()
     create_output_table()
+    populate_output_table()
 
 main()
