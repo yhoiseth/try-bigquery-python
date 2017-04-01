4b645a0291102a7d2dcffc6eba62b87a5fcc2a09
Yngve Høiseth <yhoiseth@gmail.com>
2017-04-01 15:53:18 +0200

    Print project ID

--- try.py
+++ try.py
@@ -1,1 +1,10 @@
 from google.cloud import bigquery
+from pprint import pprint
+
+bigquery_client = bigquery.Client()
+
+projects = bigquery_client.list_projects()
+
+print('Project:', bigquery_client.project)
+
+
