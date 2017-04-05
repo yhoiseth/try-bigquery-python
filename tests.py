0bd021d3a8a31979028b6cf8d9d656b15370f97d
Yngve Høiseth <yhoiseth@gmail.com>
2017-04-05 22:29:06 +0200

    Add failing test for get_table_names()

---
+++ tests.py
@@ -0,0 +1,33 @@
+import unittest
+from datetime import date
+from datetime import timedelta
+import tableutilities
+
+class TestTableUtilities(unittest.TestCase):
+    def test_get_table_names(self):
+        today = date.today()
+        one_day = timedelta(1)
+        yesterday = today - one_day
+        two_days_ago = yesterday - one_day
+        three_days_ago = two_days_ago - one_day
+
+        format = '%Y%M%D'
+
+        prefix = 'ga_sessions_'
+
+
+
+        expected_table_names = [
+            prefix + today.strftime(format),
+            prefix + yesterday.strftime(format),
+            prefix + two_days_ago.strftime(format),
+            prefix + three_days_ago.strftime(format),
+        ]
+
+        self.assertEqual(
+            expected_table_names, tableutilities.get_table_names(4)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
